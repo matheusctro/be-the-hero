9c7efdb598691f639178776d206cf87904b11838
Matheus Castro <m.s.castro@hotmail.com>
2020-03-29 10:29:43 -0400

    added verifications

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -11,5 +12,6 @@ app.use(cors()); //permite que todas as aplicações frontend possa acessar este
 // }));
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
